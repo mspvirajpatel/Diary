ffe079b586c454a964de34b2e1ce8fd59dacc34d
牛苒 <niuran@niurans-MacBook-Pro.local>
2018-02-24 18:09:51 +0800

    remove the original code from UserLoginTableViewController.swift

---
+++ Diary/Controller/UserLoginTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UserLoginTableViewController.swift
+//  Diary
+//
+//  Created by 牛苒 on 2018/2/24.
+//  Copyright © 2018年 牛苒. All rights reserved.
+//
+
+import UIKit
+
+class UserLoginTableViewController: UITableViewController {
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+}
